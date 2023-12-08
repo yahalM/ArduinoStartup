7495625102ee4183c08e273086151a472585ee75
YahalM <yahalmoshe@gmail.com>
2023-12-08 17:54:09 +0200

    HelloWrold: run first arduino code

---
+++ sketch_helloWorld/sketch_helloWorld.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // Start serial communication at a baud rate of 9600
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Print "Hello, World!" to the Serial Monitor
+  Serial.println("Hello, World yahal!");
+  delay(1000); // Wait for 1 second
+}
